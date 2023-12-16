87e858ef611a73d6aa0e4af8e2937b401dc41249
kalidas1284 <kalidas1284@gmail.com>
2023-12-16 22:12:52 +0530

    interest calculator

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import './App.css';
+import { useState } from 'react';
+
+
+function App() {
+
+const [amount,setAmount]=useState(0)
+const[year,setYear]=useState(0)
+const[rate,setRate]=useState(0)
+
+const[interest,setInterest]=useState(0)
+
+console.log(amount);
+console.log(year);
+console.log(rate);
+
+
+const calculate=(e)=>{
+      const output=amount*year*rate/100;
+      console.log(output);
+      setInterest(output)
+}
+
+
+const reset=(e)=>{
+  setAmount(0)
+  setYear(0)
+  setRate(0)
+  setInterest(0)
+}
+
+  return (
+    <div className="App">
+      <div className="container">
+
+        <div className="header">
+          <h1>Simple Interset Calculator</h1>
+          <p>Calculator Your Simple Interest With Us ❤️</p>
+        </div>
+
+<div className="total">
+  <h2>&#8377; {interest} </h2>
+  <p>Your Total Interest</p>
+</div>
+
+<div className="form">
+  <form>
+    <div className="input">
+    <TextField id="outlined-basic" value={amount||""} onChange={(e)=>setAmount(e.target.value)}  label="Amount" variant="outlined"/>
+    <br></br>
+    
+    
+     <TextField id="filled-basic" value={year||""} onChange={(e)=>setYear(e.target.value)} label="Year" variant="outlined" />
+     <br></br>
+    
+    <TextField id="standard-basic" value={rate||""}  onChange={(e)=>setRate(e.target.value)} label="Rate" variant="outlined" />
+    </div>
+
+<div className="button">
+
+<Button variant="contained" onClick={e=>calculate(e)}>Calculate</Button>
+<Button variant="outlined" onClick={e=>reset(e)}>Reset</Button>
+</div>
+
+
+  </form>
+</div>
+
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
